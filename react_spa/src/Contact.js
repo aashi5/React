d44a3a68059a1245fcf32d596a21a80558b1898d
Ashwini <ashwini.thakkar5@gmail.com>
2020-06-05 14:18:35 -0400

    Adding React single page application

---
+++ react_spa/src/Contact.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Contact extends Component{
+    render(){
+
+        return (
+            <div>
+                <h2>Contact details</h2>
+                <p>This is the contact details page.</p>
+            </div>
+        );
+    }
+}
